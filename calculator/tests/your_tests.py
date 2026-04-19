401a1e5c24e6d69c3dd00e7c0cd76c16d0fc2fc7
Hailey <haileylennox2016@gmail.com>
2026-04-19 03:41:51 +0000

    fix calculator build and tests

--- calculator/tests/your_tests.py
+++ calculator/tests/your_tests.py
@@ -1,15 +1,15 @@
-#!/usr/bin/env python3
+	#!/usr/bin/env python3
 from calculator_adapter import run
 
 
 ### ADD AT LEAST TWO TESTS HERE!
 
 def test_addition():
-        result = subprocess.run(["./calculator", "2", "+", "3"], capture_output>
+        result = run(["./calculator", "2", "+", "3"])
         assert "5" in result.stdout
 
 def test_multiplication():
-        result = subprocess.run(["./calculator", "4", "*", "5"], capture_output>
+        result = run(["./calculator", "4", "*", "5"])
         assert "20" in result.stdout
 
 
