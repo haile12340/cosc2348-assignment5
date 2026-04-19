3e6c1f651634ea3951f5c70d26f3ee8843c3ac83
Hailey <haileylennox2016@gmail.com>
2026-04-19 03:13:42 +0000

    Added Makefile and tests

--- calculator/tests/your_tests.py
+++ calculator/tests/your_tests.py
@@ -4,6 +4,14 @@ from calculator_adapter import run
 
 ### ADD AT LEAST TWO TESTS HERE!
 
+def test_addition():
+        result = subprocess.run(["./calculator", "2", "+", "3"], capture_output>
+        assert "5" in result.stdout
+
+def test_multiplication():
+        result = subprocess.run(["./calculator", "4", "*", "5"], capture_output>
+        assert "20" in result.stdout
+
 
 ###
 
